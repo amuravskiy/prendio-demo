1e23506ff20b9ea469cc82e0e3faa7deb4eb1a1d
amuravskiy <amuravskiy@solvd.com>
2023-04-05 20:53:57 +0300

    Redundant class removed

--- src/main/java/com/solvd/prendiodemo/gui/pages/DashboardPage.java
+++ src/main/java/com/solvd/prendiodemo/gui/pages/DashboardPage.java
@@ -2,30 +2,26 @@ package com.solvd.prendiodemo.gui.pages;
 
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.ExtendedWebElement;
 import com.qaprosoft.carina.core.foundation.webdriver.decorator.PageOpeningStrategy;
-import com.solvd.prendiodemo.gui.components.CartsTable;
 import com.solvd.prendiodemo.gui.components.OrdersTable;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.support.FindBy;
 
 public class DashboardPage extends BasePage {
-
-    @FindBy(xpath = "//li[@id and @class='active' and a[text()='Dashboard']]")
-    private ExtendedWebElement activeDashboardButton;
-
     @FindBy(id = "tbltoporder")
     private OrdersTable ordersTable;
 
-    @FindBy(id = "tblncart")
-    private CartsTable cartsTable;
+    @FindBy(xpath = "//div[@id='tblncart']//a[contains(text(),'View All')]")
+    private ExtendedWebElement viewAllCarts;
 
     public DashboardPage(WebDriver driver) {
         super(driver);
-        setUiLoadedMarker(activeDashboardButton);
+        setUiLoadedMarker(ordersTable.getRootExtendedElement());
         setPageOpeningStrategy(PageOpeningStrategy.BY_ELEMENT);
     }
 
     public AllCartsPage clickViewAllCarts() {
-        return cartsTable.clickViewAllButton();
+        viewAllCarts.click();
+        return new AllCartsPage(driver);
     }
 
     public String getOrderPreviewsCartName(int index) {
